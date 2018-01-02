58f56f079900d77358360e53b7eab1a8038291ee
Hemingye <961072022@qq.com>
2018-01-02 17:02:17 +0800

    123456

---
+++ src/Ming/Demo2.java
@@ -0,0 +1,18 @@
+package Ming;
+
+import java.util.Scanner;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc=new Scanner(System.in);
+     System .out.println("请输入一个数");
+ 
+     int a=sc.nextInt();
+     int[] arr=new int[a];
+     System.out.println("数组长度为");
+     System.out.println(arr.length);
+	}
+   
+}
